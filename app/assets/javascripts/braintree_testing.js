b7ef265477c5078ead20a85a52adc4c34e236406
Rob Dale <email": null>
2016-09-04 09:31:30 -0500

    Cleaned up a bit

--- app/assets/javascripts/braintree_testing.js
+++ app/assets/javascripts/braintree_testing.js
@@ -1,28 +1,14 @@
 $(document).ready(function(){
-
   clientTokenRequester();
-
-
-  // ;
-
 });
-
 
 function clientTokenRequester(){
   $.get('/client_token').done(function(response){
       nonceMaker(response);
   });
-
-
-
- // $('#braintree-form').on('submit', function(event){
- //    event.preventDefault();
-
- //  });
 }
 
 function nonceMaker(token){
-
   console.log("Starting noncemaker!");
   braintree.client.create({authorization: token}, function (clientErr, clientInstance) {
     console.log("Creating the client!");
@@ -70,7 +56,7 @@ function nonceMaker(token){
       }
 
       // $('#submit-button').removeAttribute('disabled');
-
+      console.log("Setting up the submit button!")
       $('#braintree-form').on('submit', function (event) {
         event.preventDefault();
         console.log("Button clicked!")
@@ -91,79 +77,10 @@ function nonceMaker(token){
           // document.querySelector('input[name="payment-method-nonce"]').value = payload.nonce;
           // $('#braintree-form').submit();
 
+          //the following goes to 'payments#generate_transaction'
           $.post('/transaction-endpoint', { authenticity_token: window._token, 'client-nonce': payload.nonce} )
-        });
-    });
-  // console.log('ready to submit the form!')
-  });
-});
-}
-
-// function nonceMaker(){
-//   braintree.client.create({
-//     // Replace this with your own authorization.
-//     authorization: "fkdaljfkl"  //change this to client token
-//   }, function (clientErr, clientInstance) {
-//     // if (clientErr) {
-//     //   // Handle error in client creation
-//     //   console.log("error in client creation!!!!!!")
-//     //   return;
-//     // }
-
-//     braintree.hostedFields.create({
-//       // client: clientInstance,
-//       // styles: {
-//       //   'input': {
-//       //     'font-size': '14pt'
-//       //   },
-//       //   'input.invalid': {
-//       //     'color': 'red'
-//       //   },
-//       //   'input.valid': {
-//       //     'color': 'green'
-//       //   }
-//       // },
-//       // fields: {
-//       //   number: {
-//       //     selector: '#card-number',
-//       //     placeholder: '4111 1111 1111 1111'
-//       //   },
-//       //   cvv: {
-//       //     selector: '#cvv',
-//       //     placeholder: '123'
-//       //   },
-//       //   expirationDate: {
-//       //     selector: '#expiration-date',
-//       //     placeholder: '10 / 2019'
-//       //   }
-//       // }
-//     }, function (hostedFieldsErr, hostedFieldsInstance) {
-//       // if (hostedFieldsErr) {
-//       //   // Handle error in Hosted Fields creation
-//       //   console.log("error in Hosted Fields creation!!!!!!")
-//       //   return;
-//       // }
-
-//       // submit.removeAttribute('disabled');
-
-//       // $('#braintree-form').on('submit', function (event) {
-//       //   event.preventDefault();
-
-//       //   hostedFieldsInstance.tokenize(function (tokenizeErr, payload) {
-//       //     if (tokenizeErr) {
-//       //       // Handle error in Hosted Fields tokenization
-//       //        console.log("error in Hosted Fields tokenization!!!!!!")
-//       //       return;
-//       //     }
-
-//       //     // Put `payload.nonce` into the `payment-method-nonce` input, and then
-//       //     // submit the form. Alternatively, you could send the nonce to your server
-//       //     // with AJAX.
-
-//       //     document.querySelector('input[name="payment-method-nonce"]').value = payload.nonce;
-//       //     form.submit();
-//       //   });
-//       // }, false);
-//     });
-//   });
-// };
+        }); //end of payload tokenization
+    }); //end of submit button handler
+  }); //end of hosted fields creation
+}); //end of client creation
+} // end of nonceMaker
