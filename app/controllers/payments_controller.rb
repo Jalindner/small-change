c77779f00d5d6b1c64c164095a2ccbc6246e3abf
Rob Dale <email": null>
2016-09-05 11:46:23 -0500

    updates to submission listings

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -7,37 +7,39 @@ class PaymentsController < ApplicationController
 
   def process_payment
     sub = Submission.find(params[:submission_id])
-    # if sub.votes
-    puts "Processing submission ##{sub.id}, made by #{sub.recycler} on #{sub.created_at}"
+    if sub.get_upvotes.size > sub.get_downvotes.size && sub.upvotes >=2
+      puts "Processing submission ##{sub.id}, made by #{sub.recycler} on #{sub.created_at}"
 
-    random_grant = Grant.find(rand(1..Grant.count))
-    total = 0.00
+      random_grant = Grant.find(rand(1..Grant.count))
+      total = 0.00
 
-    sub.submission_groups.each do |subm_group|
-      subm_group.quantity.times do
-        total += 0.01
+      sub.submission_groups.each do |subm_group|
+        subm_group.quantity.times do
+          total += 0.01
+        end
       end
-    end
-    puts "About to create a payment with submission_id: #{sub.id}, grant_id: #{random_grant.id}, and amount: #{total}"
+      puts "About to create a payment with submission_id: #{sub.id}, grant_id: #{random_grant.id}, and amount: #{total}"
 
-    payment = Payment.new(
-      submission_id: sub.id,
-      grant_id: random_grant.id,
-      amount: total
-      )
+      payment = Payment.new(
+        submission_id: sub.id,
+        grant_id: random_grant.id,
+        amount: total
+        )
 
-    if payment.save
-      puts "Payment saved!"
-    else
-      puts "Payment not saved!"
-      puts payment.errors.full_messages
-    end
+      if payment.save
+        puts "Payment saved!"
+      else
+        puts "Payment not saved!"
+        puts payment.errors.full_messages
+      end
 
-    random_grant.amount -= total
-    random_grant.save
-    puts "Grant reduced to #{random_grant.amount}."
-    puts "Destroying the original submission"
-    sub.destroy
-    # end # end of if sub.votes
+      random_grant.amount -= total
+      random_grant.save
+      puts "Grant reduced to #{random_grant.amount}."
+      puts "Destroying the original submission"
+      sub.destroy
+    else # end of if sub.votes
+      puts "Error: not enough votes or vote count too low."
+    end # end of if sub.votes
   end #end of process_payment
 end #end of class
