890f93c6bdbab3d035b34e7e3a18421851224e20
John Lindner <email": null>
2016-09-05 21:05:48 -0500

    add votes to your queue in /votes that are not your own

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,3 +1,10 @@
 class VotesController < ApplicationController
-
+  def index
+    random_id = rand(1..Submission.count)
+    @random_submission = Submission.find(random_id)
+    puts "______________________"
+    puts current_recycler.id
+    puts @random_submission.recycler_id
+    puts "______________________"
+  end
 end
