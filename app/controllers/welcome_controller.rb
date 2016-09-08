94b25b806591d71538a7bf58104046501aa80584
John Lindner <email": null>
2016-09-07 20:12:25 -0500

    Add highest granting sponsor on home page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -4,9 +4,16 @@ class WelcomeController < ApplicationController
   end
 
   def intro
+    @highest_grant = find_highest_grant
+    @highest_granting_sponsor = @highest_grant.sponsor
   end
 
   def whywevote
   end
+  private
 
+  def find_highest_grant
+    grants = Grant.all.order(amount: :desc)
+    return grants.first
+  end
 end
