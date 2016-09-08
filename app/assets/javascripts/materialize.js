b8338939185e289b43bbc164f94aec105ca1fa4e
Andrea Frank <franklette70@gmail.com>
2016-09-08 17:00:52 -0500

    sidebar on mobile

--- app/assets/javascripts/materialize.js
+++ app/assets/javascripts/materialize.js
@@ -2,3 +2,11 @@ function materializeTasks(){
   $('select').material_select();
   $(".dropdown-button").dropdown();
 };
+
+function sideNav() {
+$(".button-collapse").sideNav();
+};
+
+function collapseButton() {
+  $('.collapsible').collapsible();
+};
